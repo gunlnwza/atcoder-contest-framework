5e95730a3999589a01d7feff6adb546b3098600c
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2026-01-03 21:37:37 +0700

    feat: update template.py
    - overload debug()

--- contest/D.py
+++ contest/D.py
@@ -5,12 +5,18 @@
 import sys
 
 def debug(*values, **kwargs):
-    print(*values, file=sys.stderr, **kwargs)
+    def is_arr(x):
+        return type(x) in [list, tuple]
 
-def debug_arr(arr: list, **kwargs):
-    for row in arr:
-        debug(row)
-    debug(**kwargs)
+    def is_2d(x):
+        return is_arr(x) and any(is_arr(y) for y in x)
+
+    if len(values) == 1 and is_2d(values[0]):
+        for row in values[0]:
+            debug(row)
+        debug(**kwargs)
+        return
+    print(*values, file=sys.stderr, **kwargs)
 
 def ii(): return int(input())
 def ia(): return list(map(int, input().split()))
